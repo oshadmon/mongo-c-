190d293bbf498b64fc444eac08e064337d254e3e
Ori Shadmon <oshadmon@gmail.com>
2023-09-05 12:41:59 -0700

    outline for MongoDB

--- MongoDB_class.cs
+++ MongoDB_class.cs
@@ -35,98 +35,134 @@ public class MongoDB{
         var cur = client.GetDatabase(db_name);
     }
 
-    private static BsonDocument IsCollection(string, conn_info, string collection_name){
-        collectionss  database.ListCollectionNames().ToList();
+    private static bool IsCollection(IMongoDatabase, cur, string collection_name){
+        /**
+         * Check whether a collection exists
+         * 
+         * :args: 
+         *      cur:IMongoDatabase - cursor for communicating with MongoDB
+         *      collection_name:str - collection ("table") to look into
+         *  :params: 
+         *      is_collection:bool - whether the collection exists or not 
+         *      collections - collections in MongoDB 
+         *  :return: 
+         *      if collection_name in collections return True else return False
+         */
+        
+        var bool is_collection = false;
+        List<string> collections = null;
+        try {
+            collections = database.ListCollectionNames().ToList(); // list of all collections 
+        }
+        catch (Exception ex) {
+            Console.WriteLine(f"Failed to get a list of collections (Error: {ex})")
+        } finally {
+            if (collections != null && collectionNames.Contains(collection_name)) { // check whether collection exists 
+                is_collection = true;
+            }
+        }
 
+        return is_collection
     }
+    
     public static BsonDocument CheckCollection(IMongoDatabase cur, string colllection_name, str column_name, string column_value){
         /**
-         * if a row exists based on a given value 
-         * 
+         * get document (row) based on criteria -- if found need to open document 
          * :args: 
          *      cur:IMongoDatabase - cursor for communicating with MongoDB
          *      collection_name:str - collection ("table") to look into
          *      column_name:str - which column to check by 
          *      column_value:str - value to search against 
          * :params: 
-         *      collection - returned row 
+         *      is_collection:bool - whether or not collection exists 
+         *      filter:Builders - criteria to search by 
          * :return: 
-         *      if cursor is able to find row, return it, otherwise return None
+         *     if fails to locate collection returns null 
+         *     if fails to locate "row" based on criteria returns null
+         *     if able to locate "row" based on criteria returns row 
          */
+        bool is_collection = IsCollection(cur = cur, collection_name = colllection_name); 
+        
+        if (!is_collection){ // if collection doesn't exist returns null (need to create collection / file) 
+            return null; 
+        }
+        
+        // create a filter criteria based on column_name (ex. file_name) and correspondig value 
+        var filter = Builders<BsonDocument>.Filter.Eq(column_name, column_value);
 
-        //  get collection to uuse 
-        var collection = GetCollection<BsonDocument>(colllection_name);
-        iifif f 
-
-
+        // return "row" based on filter, if not found returns null
+        return collection.Find(filter).FirstOrDefault(); 
 
 
     }
 
-}
-
-using System;
-using MongoDB.Driver;
-
-public class MongoDB
-{
-    public static IMongoDatabase ConnectMongoDB(string conn_info, string db_name)
-    {
+    public static bool CreateCollection(IMongoDatabase cur, string colllection_name){
         /**
-         * Connect to MongoDB based on CONN_INFO and database
+         * Create collection for data to be stored in
          * 
          * :args:
-         *      conn_info: str - MongoDB connection information
-         *      db_name: str - database to store content in
+         *      cur:IMongoDatabase - cursor for communicating with MongoDB
+         *      collection_name:str - collection ("table") to look into
+         * :params: 
+         *      status:bool - wether collection was created
          * :return: 
-         *      cur: IMongoDatabase - cursor for communicating with MongoDB 
+         *      status
          */
+        bool status = false;
 
-        // Connect to MongoDB using the provided connection information
-        var client = new MongoClient(conn_info);
+        return status;
 
-        // Get a reference to the specified database
-        var db = client.GetDatabase(db_name);
-
-        return db;
-    }
-at
-    public static IMongoCollection<BsonDocument> GetCollection(IMongoDatabase db, string collectionName)
-    {
-        /**
-         * Get a reference to a MongoDB collection within the specified database
-         * 
-         * :args: 
-         *      db: IMongoDatabase - MongoDB database
-         *      collectionName: str - name of the collection
-         * :return: 
-         *      collection: IMongoCollection<BsonDocument> - MongoDB collection
-         */
-
-        // Get a reference to the specified collection
-        var collection = db.GetCollection<BsonDocument>(collectionName);
-
-        return collection;
     }
 
-    public static BsonDocument CheckContent(IMongoCollection<BsonDocument> collection, string name)
-    {
+
+    public static bool InsertData(IMongoDatabase cur, string colllection_name, string file_name, byte file_content){
         /**
-         * Check if a row exists based on a given value in a MongoDB collection
-         * 
+         * Write content into MongoDB 
+         * :sample row:
+         *      create_ts:datetime - create timestamp 
+         *      update_ts:datetime - update timestamp 
+         *      creator:str - person who created the file (based on windows username) 
+         *      updator:str - last person to update file (based on windows username)
+         *      file_name:str - file name 
+         *      file_content:bytes - actual file blob
          * :args: 
-         *      collection: IMongoCollection<BsonDocument> - MongoDB collection
-         *      name: str - value within a column to search by
+         *      cur:IMongoDatabase - cursor for communicating with MongoDB
+         *      collection_name:str - collection ("table") to look into
+         *      file_name:str - file name 
+         *      file_conent:bytes - file content
+         * :params: 
+         *      status:bool 
+         *      creator:str - User who created the new "row" 
+         *      timestamp:DateTime - current timestamp 
          * :return: 
-         *      content: BsonDocument - returned row if found, otherwise null
+         *      status
          */
+        bool status = false;
+        string creator = Environment.UserName; // windows user 
+        DateTime timestamp = DateTime.Now; // current timestamp 
+        
+        return status; 
 
-        // Define a filter to search for a document with the specified value in a column
-        var filter = Builders<BsonDocument>.Filter.Eq("ColumnName", name); // Replace "ColumnName" with your actual column name
+    }
 
-        // Try to find a document that matches the filter
-        var content = collection.Find(filter).FirstOrDefault();
+    public static bool UpdateData(IMongoDatabase cur, BsonDocument content){
+        /**
+         * Update MongoDB row with new content
+         * :args: 
+         *      cur:IMongoDatabase - cursor for communicating with MongoDB
+         * :params: 
+         *      status:bool 
+         *      updator:str - last person to change the file 
+         *      timestamp:DateTime - current timestamp 
+         * :return: 
+         *      status
+         */
+        bool status = false;
+        string updator = Environment.UserName; // windows user 
+        DateTime timestamp = DateTime.Now; // current timestamp (used for update row) 
 
-        return content;
+        return status;
+
+
     }
 }
